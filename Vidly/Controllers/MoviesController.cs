32baef881ea7c1e7c09c94e1980f084d8a0240af
Mouatez Kitouni <mouatezkitouni@gmail.com>
2019-11-19 18:55:35 -0500

    CommitMovieGenre

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -28,7 +28,7 @@ namespace Vidly.Controllers
             var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
-        public ActionResult Detail(int id)
+        public ActionResult Details(int id)
         {
           
             var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
